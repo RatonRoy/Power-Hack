f4efb285b6661c96a99d6231c988cc90b6808cb1
RatonRoy <royraton18@gmail.com>
2022-06-30 18:02:50 +0600

    setup files

---
+++ src/components/Pages/Table/Table.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Table.css';
+
+const Table = () => {
+	return (
+		<main>
+			<header className='header-section '>
+				<div className='header-wrap'>
+					<a className='header-item'>My Logo </a>
+					<p className='header-item'> Total Paid : </p>
+				</div>
+			</header>
+		</main>
+	);
+};
+
+export default Table;
